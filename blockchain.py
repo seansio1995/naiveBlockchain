20c92d3c869d5f63f0548bc198f885bbac4e2997
seansio1995 <seansio@163.com>
2018-07-19 18:20:54 +0800

    add proof of work mechanism

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,7 @@
 import functools
+import hashlib as hl
+import json
+from hash_util import hash_string_256, hash_block
 #Initialize the mining reward for block miner
 MINING_REWARD = 10
 
@@ -6,13 +9,43 @@ MINING_REWARD = 10
 genesis_block={
 "previous_hash":"",
 "index":0,
-"transactions":[]
+"transactions":[],
+"proof":100
 }
 blockchain = []
 blockchain.append(genesis_block)
 open_transactions=[]
 owner="Max"
 participants={"Max"}
+
+
+def valid_proof(transactions, last_hash, proof):
+    """Validate a proof of work number and see if it solves the puzzle algorithm (two leading 0s)
+
+    Arguments:
+        :transactions: The transactions of the block for which the proof is created.
+        :last_hash: The previous block's hash which will be stored in the current block.
+        :proof: The proof number we're testing.
+    """
+    # Create a string with all the hash inputs
+    guess = (str(transactions) + str(last_hash) + str(proof)).encode()
+    # Hash the string
+    # IMPORTANT: This is NOT the same hash as will be stored in the previous_hash. It's a not a block's hash. It's only used for the proof-of-work algorithm.
+    guess_hash = hash_string_256(guess)
+    print(guess_hash)
+    # Only a hash (which is based on the above inputs) which starts with two 0s is treated as valid
+    # This condition is of course defined by you. You could also require 10 leading 0s - this would take significantly longer (and this allows you to control the speed at which new blocks can be added)
+    return guess_hash[0:2] == '00'
+
+
+def proof_of_work():
+    last_block=blockchain[-1]
+    last_hash=hash_block(last_block)
+    proof=0
+
+    while not valid_proof(open_transactions,last_hash,proof):
+        proof+=1
+    return proof
 
 def get_last_blockchain_value():
     """ Returns the last value of the current blockchain. """
@@ -42,8 +75,8 @@ def add_transaction(recipient,sender=owner, amount=1.0):
         return True
     return False
 
-def hash_block(block):
-    return "-".join([str(block[key]) for key in block])
+# def hash_block(block):
+#     return "-".join([str(block[key]) for key in block])
 
 def get_balance(participant):
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
@@ -79,18 +112,16 @@ def mine_block():
     last_block=blockchain[-1]
     hashed_block=hash_block(last_block)
     #print(hashed_block)
-    reward_transaction={
-    "sender":"MINING",
-    "recipient":owner,
-    "amount":MINING_REWARD
-    }
-
+    proof = proof_of_work()
+    reward_transaction=OrderedDict(
+        [('sender', 'MINING'), ('recipient', owner), ('amount', MINING_REWARD)])
     copied_transactions=open_transactions[:]
     copied_transactions.append(reward_transaction)
     block={
     "previous_hash":hashed_block,
     "index": len(blockchain),
-    "transactions":copied_transactions
+    "transactions":copied_transactions,
+    "proof":proof
     }
     blockchain.append(block)
     return True
