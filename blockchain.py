ffa81ce8a536888c5bbc725acbeaa78448596e83
seansio1995 <seansio@163.com>
2018-07-19 15:00:46 +0800

    add mine_block functionality

--- blockchain.py
+++ blockchain.py
@@ -34,6 +34,8 @@ def add_transaction(recipient,sender=owner, amount=1.0):
 
 def mine_block():
     last_block=blockchain[-1]
+    hashed_block="-".join(str([last_block[key]) for key in last_block])
+    print(hashed_block)
     block={
     "previous_hash":"XYZ",
     "index": len(blockchain),
@@ -100,7 +102,8 @@ waiting_for_input = True
 while waiting_for_input:
     print('Please choose')
     print('1: Add a new transaction value')
-    print('2: Output the blockchain blocks')
+    print("2: Hash the new block")
+    print('3: Output the blockchain blocks')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -110,7 +113,9 @@ while waiting_for_input:
         recipient,amount=tx_data
         add_transaction(recipient,amount=amount)
         print(open_transactions)
-    elif user_choice == '2':
+    elif user_choice=="2":
+        mine_block()
+    elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == 'h':
         # Make sure that you don't try to "hack" the blockchain if it's empty
@@ -121,11 +126,11 @@ while waiting_for_input:
         waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
-    if not verify_chain():
-        print_blockchain_elements()
-        print('Invalid blockchain!')
-        # Break out of the loop
-        break
+    # if not verify_chain():
+    #     print_blockchain_elements()
+    #     print('Invalid blockchain!')
+    #     # Break out of the loop
+    #     break
 else:
     print('User left!')
 
